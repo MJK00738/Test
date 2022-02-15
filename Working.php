34c3e47c0acf52bac95926640d9384eb8122c22b
Muhammad Junaid Khan <junaid.khan@codenterprise.com>
2022-02-15 17:51:07 +0500

    addin file in branch of mergin branch

---
+++ Working.php
@@ -0,0 +1,2 @@
+a working file to be added to master
+Testing on the file which is to be merged in main
--- user.php
+++ user.php
@@ -1,2 +1,4 @@
 testings
 SubBranch Concept
+
+Testing on the concept of merging branch and branch from branch
